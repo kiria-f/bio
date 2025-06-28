08ec7a75bfaa1edb32dbeae26bfb0673a1873e9a
Fedor <fedor122448@gmail.com>
2025-06-29 03:03:19 +0400

    Add simple hover effect

--- lib/components/fk_button.dart
+++ lib/components/fk_button.dart
@@ -1,4 +1,5 @@
 import 'package:bio/constants/constants.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/widgets.dart';
 
 class FKButton extends StatefulWidget {
@@ -11,100 +12,153 @@ class FKButton extends StatefulWidget {
   State<FKButton> createState() => _FKButtonState();
 }
 
-class _FKButtonState extends State<FKButton>
-    with SingleTickerProviderStateMixin {
-  late final AnimationController controller;
-  late final Animation<double> elevation;
-  late bool pendingRelease;
+class _FKButtonState extends State<FKButton> with TickerProviderStateMixin {
+  late final AnimationController elevationController;
+  late final Animation<double> elevationTween;
+  late final AnimationController dimController;
+  late final Animation<double> dimTween;
+  late bool pressed;
+  late bool hovered;
 
   @override
   void initState() {
     super.initState();
-    controller = AnimationController(vsync: this);
-    elevation = Tween<double>(begin: 1, end: 0).animate(controller);
-    pendingRelease = false;
+    elevationController = AnimationController(vsync: this);
+    elevationTween = Tween<double>(
+      begin: 1,
+      end: 0,
+    ).animate(elevationController);
+    dimController = AnimationController(vsync: this);
+    dimTween = Tween<double>(begin: 1, end: 0).animate(dimController);
+    pressed = false;
+    hovered = false;
   }
 
   @override
   void dispose() {
-    pendingRelease = false;
-    controller.dispose();
+    pressed = false;
+    hovered = false;
+    elevationController.dispose();
+    dimController.dispose();
     super.dispose();
   }
 
-  TickerFuture animatePush() {
-    return controller.animateTo(1, duration: const Duration(milliseconds: 50));
+  TickerFuture animatePress() {
+    return elevationController.animateTo(
+      1,
+      duration: const Duration(milliseconds: 50),
+    );
   }
 
   TickerFuture animateRelease() {
-    return controller.animateTo(
+    return elevationController.animateTo(
       0,
       duration: const Duration(milliseconds: 500),
       curve: Curves.easeOutQuad,
     );
   }
 
+  TickerFuture animateEnter() {
+    return dimController.animateTo(
+      1,
+      duration: const Duration(milliseconds: 100),
+      curve: Curves.linear,
+    );
+  }
+
+  TickerFuture animateExit() {
+    return dimController.animateTo(
+      0,
+      duration: const Duration(milliseconds: 100),
+      curve: Curves.linear,
+    );
+  }
+
   void onPointerDown(PointerDownEvent event) async {
+    pressed = true;
     if (widget.onPressed != null) {
       widget.onPressed!(event);
     }
-    controller.stop();
-    await animatePush();
-    if (pendingRelease) {
-      pendingRelease = false;
+    elevationController.stop();
+    await animatePress();
+    if (!pressed) {
       animateRelease();
     }
   }
 
   void onPointerUp(PointerUpEvent event) async {
-    if (controller.isAnimating) {
-      pendingRelease = true;
-    } else {
+    pressed = false;
+    if (!elevationController.isAnimating) {
       animateRelease();
     }
   }
 
+  void onEnter(PointerEnterEvent event) async {
+    hovered = true;
+    animateEnter();
+  }
+
+  void onExit(PointerExitEvent event) async {
+    hovered = false;
+    animateExit();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Listener(
-      onPointerDown: onPointerDown,
-      onPointerUp: onPointerUp,
-      child: AnimatedBuilder(
-        animation: elevation,
-        builder: (context, child) {
-          return Transform.translate(
-            offset: Offset(0, 3 * (1 - elevation.value)),
-            child: Container(
-              decoration: BoxDecoration(
-                color: Color.fromARGB(255, 255, 255, 255),
-                borderRadius: BorderRadius.circular(1000),
-                boxShadow: [
-                  BoxShadow(
-                    color: Color.from(
-                      alpha: 0.5 + (1 - elevation.value) / 2,
-                      red: 0,
-                      green: 0,
-                      blue: 0,
+    return MouseRegion(
+      cursor: SystemMouseCursors.click,
+      onEnter: onEnter,
+      onExit: onExit,
+      child: Listener(
+        onPointerDown: onPointerDown,
+        onPointerUp: onPointerUp,
+        child: AnimatedBuilder(
+          animation: elevationTween,
+          builder: (context, externalChild) {
+            return Transform.translate(
+              offset: Offset(0, 3 * (1 - elevationTween.value)),
+              child: AnimatedBuilder(
+                animation: dimTween,
+                builder: (context, internalChild) {
+                  return Container(
+                    decoration: BoxDecoration(
+                      color: Color.from(
+                        alpha: 1,
+                        red: 1 - (1 - dimTween.value) / 100,
+                        green: 1 - (1 - dimTween.value) / 100,
+                        blue: 1 - (1 - dimTween.value) / 100,
+                      ),
+                      borderRadius: BorderRadius.circular(1000),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Color.from(
+                            alpha: 0.5 + (1 - elevationTween.value) / 2,
+                            red: 0,
+                            green: 0,
+                            blue: 0,
+                          ),
+                          blurRadius: 3 * elevationTween.value,
+                          spreadRadius: 0 - (1 - elevationTween.value) * 1,
+                          offset: Offset(0, 3 * elevationTween.value),
+                        ),
+                      ],
                     ),
-                    blurRadius: 3 * elevation.value,
-                    spreadRadius: 0 - (1 - elevation.value) * 1,
-                    offset: Offset(0, 3 * elevation.value),
-                  ),
-                ],
+                    child: externalChild,
+                  );
+                },
               ),
-              child: child,
-            ),
-          );
-        },
-        child: Container(
-          margin: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-          child: Text(
-            widget.label,
-            style: TextStyle(
-              fontFamily: 'Source Code Pro',
-              fontSize: Constants.sizes.font.normal,
-              fontWeight: FontWeight.w800,
-              color: const Color.fromARGB(255, 0, 0, 0),
+            );
+          },
+          child: Container(
+            margin: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+            child: Text(
+              widget.label,
+              style: TextStyle(
+                fontFamily: 'Source Code Pro',
+                fontSize: Constants.sizes.font.normal,
+                fontWeight: FontWeight.w800,
+                color: const Color.fromARGB(255, 0, 0, 0),
+              ),
             ),
           ),
         ),
